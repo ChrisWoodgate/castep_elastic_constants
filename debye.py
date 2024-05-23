633235bf36b9198ab4fcc53f1ffdf565c459c7d2
Chris Woodgate <c.woodgate@warwick.ac.uk>
2024-05-23 22:19:15 +0100

    File for routines calculating sound velocities and Debye temperatures.

---
+++ debye.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+debye.py
+
+Calculate (estimated) sound velocities and Debye temperatures according
+to form proposed by Anderson [J. Phys. Chem. Solids 24, 909 (1963)].
+
+Christopher D. Woodgate, 2024
+
+"""
+
+import numpy as np
+import CijUtil
+
+def AndersonDebye(Cij, eCij, outputfile, nt=False):
+    """Use calculated Hill bulk and shear moduli and return
+       estimates of the polycrystalline longitudinal and transverse
+       sound velocities and (consequently) the Debye temperature."""
+
+    # TODO
+    # 1. Carry across code from project Jupyter Notebook
+    # 2. Think about error propogation?
+
+    return None
+    
